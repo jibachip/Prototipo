49af1411799aee2b01d48fd369f820cd9e54b6eb
jibachip <jibachip@gmail.com>
2017-12-02 15:56:10 -0600

    Layout hasta registro datos generales estacionamiento

--- app/src/main/java/com/example/android/prototipo/RegistroTab.java
+++ app/src/main/java/com/example/android/prototipo/RegistroTab.java
@@ -19,7 +19,7 @@ public class RegistroTab extends AppCompatActivity {
     int t1;
 
         TabHost tabHost;
-        EditText correo,usuario,contraseña,confcontraseña,nombre,apPat,apMat,fechaNac;
+        EditText correo,usuario,contraseña,confcontraseña,nombre,apPat,apMat,fechaNac,placas;
         Spinner genero;
         RadioButton cliente,dueño;
     @Override
@@ -38,13 +38,14 @@ public class RegistroTab extends AppCompatActivity {
        genero=findViewById(R.id.spGenero);
        cliente=findViewById(R.id.rbCliente);
        dueño=findViewById(R.id.rbDueño);
+        placas=findViewById(R.id.etMatricula);
 
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(getApplicationContext(),R.array.Genero,R.layout.support_simple_spinner_dropdown_item);
         genero.setAdapter(adapter);
 
     final Button btntab2=findViewById(R.id.p1p2);
     final Button btntab3=findViewById(R.id.p2p3);
-
+    final Button btnRegisCliente=findViewById(R.id.btnRegCliente);
 
     //Creacion tabhost
        tabHost=findViewById(R.id.tabhost);
@@ -64,14 +65,23 @@ public class RegistroTab extends AppCompatActivity {
 //Agrega tercera pestaña.
         spec=tabHost.newTabSpec("Tab Three");
         spec.setContent(R.id.tab3);
-        spec.setIndicator("Tab Three");
+        spec.setIndicator("Cliente");
         tabHost.addTab(spec);
+//Agrega cuarta pestaña
+        spec=tabHost.newTabSpec("Tab Four");
+        spec.setContent(R.id.tab4);
+        spec.setIndicator("Estacionamiento");
+        tabHost.addTab(spec);
+
+        tabHost.getTabWidget().getChildTabViewAt(0).setEnabled(false);
         tabHost.getTabWidget().getChildTabViewAt(1).setVisibility(View.INVISIBLE);
         tabHost.getTabWidget().getChildTabViewAt(2).setVisibility(View.INVISIBLE);
-        tabHost.getTabWidget().getChildTabViewAt(0).setEnabled(false);
         tabHost.getTabWidget().getChildTabViewAt(1).setEnabled(false);
         tabHost.getTabWidget().getChildTabViewAt(1).getLayoutParams().width=60;
         tabHost.getTabWidget().getChildTabViewAt(2).setEnabled(false);
+        tabHost.getTabWidget().getChildTabViewAt(3).setVisibility(View.INVISIBLE);
+        tabHost.getTabWidget().getChildTabViewAt(3).setEnabled(false);
+
 
         
         
@@ -96,9 +106,29 @@ public class RegistroTab extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (nombre.getText().length()!=0 && apPat.getText().length()!=0 && apMat.getText().length()!=0
-                        && fechaNac.getText().length()!=0 && genero.getSelectedItem()!=null){
+                        && fechaNac.getText().length()!=0 && genero.getSelectedItem()!=null ){
+
+                    if (cliente.isChecked()){
                     tabHost.getTabWidget().getChildTabViewAt(2).setVisibility(View.VISIBLE);
-                    tabHost.setCurrentTabByTag("Tab Three");
+                    tabHost.setCurrentTabByTag("Tab Three");}
+                    else if (dueño.isChecked()){
+                        tabHost.getTabWidget().getChildTabViewAt(2).setVisibility(View.GONE);
+                        tabHost.getTabWidget().getChildTabViewAt(3).setVisibility(View.VISIBLE);
+                        tabHost.setCurrentTabByTag("Tab Four");
+                    }
+                }
+                else{
+                    genToast("Faltan datos");
+                }
+            }
+        });
+
+        btnRegisCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (placas.getText().length()!=0) {
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                    startActivity(intent);
                 }
                 else{
                     genToast("Faltan datos");
@@ -123,6 +153,7 @@ public void genToast(String mensaje){
             case 0:super.onBackPressed();break;
             case 1: tabHost.setCurrentTabByTag("Tab One");break;
             case 2:tabHost.setCurrentTabByTag("Tab Two");break;
+            case 3:tabHost.setCurrentTabByTag("Tab Three");
         }
     }
 }
